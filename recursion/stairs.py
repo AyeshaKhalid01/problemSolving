29b1b0d8309a1d87188d5a84ee41fd5c076741c9
AyeshaKhalid01 <ayeshakhalidsh@gmail.com>
2023-06-04 21:25:21 -0700

    stairs mem

---
+++ recursion/stairs.py
@@ -0,0 +1,11 @@
+memo={}
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n in memo:
+            return memo[n]
+        if (n==0):
+            return 1
+        if (n<0):
+            return 0
+        memo[n]= (self.climbStairs(n-1))+(self.climbStairs(n-2))
+        return memo[n]
